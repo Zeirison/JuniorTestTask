6907cf481b17bca60a700cf0afe268725e8c8c11
Zeirison <zeirison@gmail.com>
2021-03-08 16:45:06 +0200

    Revert "Code review"

--- app/src/main/java/com/nazarrybickij/juniortesttask/network/ApiHelper.kt
+++
@@ -1,5 +0,0 @@
-package com.nazarrybickij.juniortesttask.network
-
-class ApiHelper(private val apiService: ApiService) {
-    suspend fun getCars(count:Int, key:String) = apiService.getCars(count, key)
-}
